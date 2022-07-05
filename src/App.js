2af3cfcc554fb9fc92ba9ef628116fba0bc365d6
nik's <rathodnikunj200@gmail.com>
2022-07-05 11:15:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Row,Container,Col,Card,Table} from 'react-bootstrap';
+import React,{useState, useEffect} from 'react';
 
 function App() {
+  const [mydata,setData]= useState([]);
+  const apiGet = () => {
+    fetch('https://inshorts-api.herokuapp.com/news?category=sports')
+    .then((response)=>response.json())
+    .then((json)=>{
+      console.log(json);
+      setData(json.data);
+    });
+  };
+
+  useEffect(()=>{
+    apiGet();
+    const interval =setInterval(()=>{
+      apiGet();
+    },50000);
+    return () => clearInterval(interval);
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container fluid >
+      <Row xs={1} md={3} className="g-4">
+        {
+          mydata.map(
+            (value)=>{
+              return(
+                <>
+                  <Col className="container-fluid mt-4">
+                    <Card border="success">
+                    <Card.Img varient="top" height="350px" width="50%" src={value.imageUrl} />
+                    <Card.Body>
+                    <Card.Title>{value.date}</Card.Title>
+                    <Card.Text>{value.content}</Card.Text>
+                    </Card.Body>
+                    </Card>
+                  </Col>
+                </>                
+              )
+            }
+          )
+        }
+      </Row>
+    </Container>
+  )
 }
 
 export default App;
---
+++ src/custom.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Custom(){
+    return(
+        <div>
+            <center><h1><emp> Live News</emp></h1></center>
+        </div>
+    );
+}
+export default Custom;
